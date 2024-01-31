cdd6c179b6da2699431f2e2846ff640be4b13401
sebhajek <seb.hajek@gmail.com>
2024-01-31 18:52:31 +0100

    added git in

--- get.sh
+++ get.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.setup-dots/dots;
 cd ~/.setup-dots/dots;
+zypper in git;
 git clone https://github.com/sebhajek/dots.git ~/.setup-dots/dots;
 git submodule update --recursive --remote;
 
