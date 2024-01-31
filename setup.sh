5a37f5dac3d84a1a717ea4f35e7dafcf397fc8cd
sebhajek <seb.hajek@gmail.com>
2024-01-31 20:06:02 +0100

    extra non interactive

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo zypper -n in -tpackage --no-recommends foot tmux;
 
 sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
 sudo zypper refresh;
-sudo zypper dup --from packman-essentials --allow-vendor-change;
+sudo zypper -n dup --from packman-essentials --allow-vendor-change;
 sudo zypper -n in ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
 sudo zypper -n in firefox;
 sudo zypper -n in gh;
