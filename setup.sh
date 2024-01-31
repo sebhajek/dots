532274db97b6d8601790184ff58949eb6084e234
sebhajek <seb.hajek@gmail.com>
2024-01-31 19:21:35 +0100

    added gh package

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo zypper refresh;
 sudo zypper in --non-interactive curl;
 sudo zypper in --non-interactive git fish opi nano;
 sudo zypper in --no-recommends luajit;
-sudo zypper in --non-interactive git nmcli;
+sudo zypper in --non-interactive nmcli;
 sudo zypper in -tpackage --no-recommends --non-interactive sway-branding-upstream sway swaybar swayidle swaylock;
 sudo zypper in -tpackage --no-recommends --non-interactive greetd gtkgreet;
 sudo zypper in -tpackage --no-recommends --non-interactive foot tmux;
@@ -13,3 +13,6 @@ sudo zypper refresh;
 sudo zypper dup --from packman-essentials --allow-vendor-change;
 sudo zypper in --non-interactive ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
 sudo zypper in --non-interactive firefox;
+sudo zypper in --non-interactive gh;
+
+bash ~/.setup-dots/dots/wm/setup.sh
