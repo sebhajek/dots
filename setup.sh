33a80d895002d5eb855a372173f7ca50b35a9ad0
sebhajek <seb.hajek@gmail.com>
2024-01-31 19:02:49 +0100

    added non interactives

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 sudo zypper refresh;
-sudo zypper in curl;
-sudo zypper in git fish opi nano;
+sudo zypper in --non-interactive curl;
+sudo zypper in --non-interactive git fish opi nano;
 sudo zypper in --no-recommends luajit;
-sudo zypper in git nmcli;
-sudo zypper in -tpackage --no-recommends sway-branding-upstream sway swaybar swayidle swaylock;
-sudo zypper in -tpackage --no-recommends greetd gtkgreet;
-sudo zypper in -tpackage --no-recommends foot tmux;
+sudo zypper in --non-interactive git nmcli;
+sudo zypper in -tpackage --no-recommends --non-interactive sway-branding-upstream sway swaybar swayidle swaylock;
+sudo zypper in -tpackage --no-recommends --non-interactive greetd gtkgreet;
+sudo zypper in -tpackage --no-recommends --non-interactive foot tmux;
 
 sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
 sudo zypper refresh;
 sudo zypper dup --from packman-essentials --allow-vendor-change;
-sudo zypper install ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
-sudo zypper in firefox;
+sudo zypper in --non-interactive ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
+sudo zypper in --non-interactive firefox;
