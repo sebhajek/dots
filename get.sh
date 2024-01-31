fbf7b902f5d6ecc0ed951dbf91339ad2c1eefb27
sebhajek <seb.hajek@gmail.com>
2024-01-31 18:48:55 +0100

    fixed mkdir

--- get.sh
+++ get.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -pf ~/.setup-dots/dots;
+mkdir -p ~/.setup-dots/dots;
 cd ~/.setup-dots/dots;
 git clone https://github.com/sebhajek/dots.git ~/.setup-dots/dots;
 git submodule update --recursive --remote;
