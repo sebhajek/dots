c9170436f1b8e68eb8f74898ed1cb351f7b551c5
sebhajek <seb.hajek@gmail.com>
2024-02-02 20:12:28 +0100

    GPG!!!!

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo zypper -n in -tpackage --no-recommends greetd gtkgreet;
 sudo zypper -n in -tpackage --no-recommends foot tmux;
 
 sudo zypper ar -cfp 99 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
-sudo zypper refresh --gpg-auto-import-keys;
+sudo zypper refresh | echo 'a';
 sudo zypper -n dup --from packman-essentials --allow-vendor-change;
 sudo zypper -n in ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
 sudo zypper -n in firefox;
