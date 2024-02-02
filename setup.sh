84877af4f51c41911b9129faf161f69d2a2f08fc
sebhajek <seb.hajek@gmail.com>
2024-02-02 20:08:53 +0100

    fixed gpt imports

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ sudo zypper -n in -tpackage --no-recommends sway-branding-upstream sway swayidle
 sudo zypper -n in -tpackage --no-recommends greetd gtkgreet;
 sudo zypper -n in -tpackage --no-recommends foot tmux;
 
-sudo zypper ar --gpg-auto-import-keys -cfp 99 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
-sudo zypper refresh;
+sudo zypper ar -cfp 99 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
+sudo zypper refresh --gpg-auto-import-keys;
 sudo zypper -n dup --from packman-essentials --allow-vendor-change;
 sudo zypper -n in ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
 sudo zypper -n in firefox;
