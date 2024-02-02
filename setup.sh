096cf374b7ff515547bc436bd04dd2928026b5ed
sebhajek <seb.hajek@gmail.com>
2024-02-02 20:26:40 +0100

    gpg

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo zypper refresh --no-gpg-checks;
+sudo zypper refresh;
 sudo zypper -n in curl wget 7zip;
 sudo zypper -n in git fish opi nano;
 sudo zypper -n remove pulseaudio;
@@ -9,9 +9,10 @@ sudo zypper -n in -tpackage --no-recommends sway-branding-upstream sway swayidle
 sudo zypper -n in -tpackage --no-recommends greetd gtkgreet;
 sudo zypper -n in -tpackage --no-recommends foot tmux;
 
-sudo zypper ar -cfp 99 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
-sudo zypper refresh --no-gpg-checks;
-sudo zypper -n dup --from packman-essentials --allow-vendor-change;
+sudo zypper rr packmanessentials;
+sudo zypper ar -cfp 99 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packmanessentials;
+sudo zypper --gpg-auto-import-keys ref;
+sudo zypper -n dup --from packmanessentials --allow-vendor-change;
 sudo zypper -n in ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
 sudo zypper -n in firefox;
 sudo zypper -n in gh;
