de28393196d5f9a9e87be0dd3ffc15abecc38432
sebhajek <seb.hajek@gmail.com>
2024-02-02 20:18:39 +0100

    gpg

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo zypper refresh;
+sudo zypper refresh --gpg-auto-import-keys;
 sudo zypper -n in curl wget 7zip;
 sudo zypper -n in git fish opi nano;
 sudo zypper -n remove pulseaudio;
@@ -10,7 +10,7 @@ sudo zypper -n in -tpackage --no-recommends greetd gtkgreet;
 sudo zypper -n in -tpackage --no-recommends foot tmux;
 
 sudo zypper ar -cfp 99 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials;
-sudo zypper refresh --no-gpg-checks;
+sudo zypper refresh --gpg-auto-import-keys;
 sudo zypper -n dup --from packman-essentials --allow-vendor-change;
 sudo zypper -n in ffmpeg libavcodec-full vlc-codecs pipewire-aptx gstreamer-plugins-bad-codecs gstreamer-plugins-ugly-codecs gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-good-extra gstreamer-plugins-bad gstreamer-plugins-ugly;
 sudo zypper -n in firefox;
